c333f7178d6d67fa1e70b7568a79a675c0535150
Alessio <72412290+ale89-pit@users.noreply.github.com>
2023-05-12 12:40:28 +0200

    aa

--- W2D5_Project/src/w2D5_Project/MainProject.java
+++ W2D5_Project/src/w2D5_Project/MainProject.java
@@ -4,17 +4,34 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MainProject {
+	public static Scanner input = new Scanner(System.in); 
 	static Logger log = LoggerFactory.getLogger(MainProject.class);
 	public static void main(String[] args) {
 		log.info("test");
 		System.out.println(addELement());
 	}
 public static ElementoBiblio addELement(){
+	System.out.println("Vuoi inserire una rivista->1 o un libro->2");
+	int risp = Integer.parseInt(input.nextLine());
+	if(risp == 1) {
+		System.out.println("Insericsci ISBN");
+		String isbn= input.nextLine();
+		System.out.println("Insericsci Titolo");
+		String title= input.nextLine();
+		System.out.println("Insericsci Data Pubblicazione");
+		String pubb= input.nextLine();
+		System.out.println("Insericsci ");
+		String isbn= input.nextLine();
+		System.out.println("Insericsci ISBN");
+		String isbn= input.nextLine();
+		
+	}
 	ElementoBiblio element = new Book("1234fesa4","titolo", LocalDate.of(1920, 10, 02),150,"pippo","fantasy");
 //	element.add();
 //	element.add(new Rivista("dffs332","titolo2", LocalDate.of(2020, 05, 30),50,Periodicita.MENSILE));
